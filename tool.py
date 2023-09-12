4d82ac8ff0b8de47918161d07a8eaf385c76a8b0
tanyeye1 <1552467926@qq.com>
2023-09-12 14:14:33 +0800

    add photos

--- tool.py
+++ tool.py
@@ -168,7 +168,9 @@ def git_operation():
     '''
     os.system('git add --all')
     os.system('git commit -m "add photos"')
-    os.system('git push gitee master')
+    # os.system('git push gitee master')
+    os.system('git push origin master')
+
 
 if __name__ == "__main__":
     cut_photo()        # 裁剪图片，裁剪成正方形，去中间部分
